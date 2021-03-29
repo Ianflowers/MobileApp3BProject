f7c967290744c8e75361ecb18dd331270cc1b81a
Jackson McKinzie <jackson.mckinzie.k12.friscoisd.org>
2021-03-29 14:02:56 -0500

    Added Scoreboard Class

---
+++ AirHockey/Scoreboard.swift
@@ -0,0 +1,16 @@
+//
+//  Scoreboard.swift
+//  AirHockey
+//
+//  Created by McKinzie, Jackson D on 3/29/21.
+//
+
+import Foundation
+
+class Scoreboard {
+    
+    init() {
+        
+    }
+    
+}
