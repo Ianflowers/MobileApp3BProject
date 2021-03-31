b0223671732a489dfaddc2644c9450be06a26528
Jackson McKinzie <jackson.mckinzie.k12.friscoisd.org>
2021-03-31 14:06:34 -0500

    MORE CHANGES TO SCOREBOARD

--- AirHockey/Scoreboard.swift
+++ AirHockey/Scoreboard.swift
@@ -14,19 +14,27 @@ class Scoreboard {
     var playerTwoScoreLabel: UILabel
     var playerOneScore: Int
     var playerTwoScore: Int
+    let screen = UIScreen.main.bounds
+    let screenWidth: CGFloat
+    let screenHeight: CGFloat
     
     init() {
+        screenWidth = screen.size.width
+        screenHeight = screen.size.height
+        
+        
         playerOneScore = 0
         playerTwoScore = 0
         
         playerOneScoreLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
         playerOneScoreLabel.center = CGPoint(x: 160, y: 285)
         playerOneScoreLabel.textAlignment = .center
-        
+        playerOneScoreLabel.transform = CGAffineTransform(rotationAngle: CGFloat.pi / 2)
         
         playerTwoScoreLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
-        playerOneScoreLabel.center = CGPoint(x: 160, y: 285)
-        playerOneScoreLabel.textAlignment = .center
+        playerTwoScoreLabel.center = CGPoint(x: 160, y: 285)
+        playerTwoScoreLabel.textAlignment = .center
+        playerTwoScoreLabel.transform = CGAffineTransform(rotationAngle: CGFloat.pi / 2)
         
         
         
@@ -34,6 +42,8 @@ class Scoreboard {
         playerTwoScoreLabel.text = String(playerTwoScore)
         
         //self.view.addSubview(label)
+        
+        //
     }
     
 }
