fe505bc524541be9ad676d9a0198e889b442da0c
Jackson McKinzie <jackson.mckinzie.k12.friscoisd.org>
2021-03-31 13:36:34 -0500

    Changes to scoreboard

--- AirHockey/Scoreboard.swift
+++ AirHockey/Scoreboard.swift
@@ -6,11 +6,34 @@
 //
 
 import Foundation
+import UIKit
 
 class Scoreboard {
     
+    var playerOneScoreLabel: UILabel
+    var playerTwoScoreLabel: UILabel
+    var playerOneScore: Int
+    var playerTwoScore: Int
+    
     init() {
+        playerOneScore = 0
+        playerTwoScore = 0
         
+        playerOneScoreLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
+        playerOneScoreLabel.center = CGPoint(x: 160, y: 285)
+        playerOneScoreLabel.textAlignment = .center
+        
+        
+        playerTwoScoreLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
+        playerOneScoreLabel.center = CGPoint(x: 160, y: 285)
+        playerOneScoreLabel.textAlignment = .center
+        
+        
+        
+        playerOneScoreLabel.text = String(playerOneScore)
+        playerTwoScoreLabel.text = String(playerTwoScore)
+        
+        //self.view.addSubview(label)
     }
     
 }
